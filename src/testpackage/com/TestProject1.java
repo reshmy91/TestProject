449d6670a4004815f4d0205b7da8b16d56891de3
reshmy91 <reshmyrnair123@gmail.com>
2023-10-17 23:17:52 -0500

    Added new line

--- src/testpackage/com/TestProject1.java
+++ src/testpackage/com/TestProject1.java
@@ -4,11 +4,9 @@ public class TestProject1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-	{
-		
+		System.out.println("Hi");
+		System.out.println("Hello");
+		System.out.println("How are you");
 	}
 
 }
-
